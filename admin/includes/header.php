028b66ad60d1c4a038da9a35eeaa78de92bb7107
apondi-911 <apondiashley2@gmail.com>
2024-11-14 17:05:29 +0300

    add new files

---
+++ admin/includes/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ash's Portfolio</title>
+  <!-- base:css -->
+  <link rel="stylesheet" href="assets/vendors/typicons/typicons.css">
+  <link rel="stylesheet" href="assets/vendors/css/vendor.bundle.base.css">
+  <!-- endinject -->
+  <!-- plugin css for this page -->
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="assets/css/style.css">
+  <!-- endinject -->
+  <link rel="shortcut icon" href="assets/images/favicon.ico" />
+</head>
+<body>
+   <nav>
+
+   </nav>
+</body>
+</html>
---
+++ admin/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Portfolio Admin</title>
+  <?php
+  include 'includes/header.php';
+
+  ?>
+</head>
+<body>
+ <?php
+ include 'dashboard.php';
+
+ ?>
+  <!-- container-scroller -->
+
+</body>
+
+</html>
+
